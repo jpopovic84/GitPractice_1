04d38341cc03db8a1bc59c34d6b63ae0bbeab116
jpopovic84 <jpopovic984@gmail.com>
2021-07-26 11:31:50 -0400

    added more work

--- src/day1/SecondClass.java
+++ src/day1/SecondClass.java
@@ -2,6 +2,11 @@ package day1;
 
 public class SecondClass {
     public static void main(String[] args) {
+        // work 1 is done
+        // work 2 is done now
+        // work 3 is done
+        // work 4 is done
+        // work 5 is done as well
 
     }
 
