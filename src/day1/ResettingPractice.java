4836191f5ff465a19644bad38c8744d2640fdd97
jpopovic84 <jpopovic984@gmail.com>
2021-07-26 12:37:05 -0400

    Test case part1 is done

---
+++ src/day1/ResettingPractice.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class ResettingPractice {
+
+    public static void main(String[] args) {
+
+        // Test Case part1 is done
+        // Test Case part2 is done
+        // Test Case part3 is done
+
+
+    }
+}
